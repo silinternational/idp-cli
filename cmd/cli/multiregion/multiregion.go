47e1672af139fbcdac0d48b046f555f062e4fbac
briskt <3172830+briskt@users.noreply.github.com>
2023-08-04 14:39:41 -0600

    fix compile-time bug

--- cmd/cli/multiregion/multiregion.go
+++ cmd/cli/multiregion/multiregion.go
@@ -33,8 +33,8 @@ func SetupMultiregionCmd(parentCommand *cobra.Command) {
 	flags.NewStringFlag(multiregionCmd, flags.Env, "", envProd, "Execution environment")
 	flags.NewStringFlag(multiregionCmd, flags.Region2, "", "", "Secondary AWS region")
 	flags.NewStringFlag(multiregionCmd, flags.TfcToken, "", "", "Token for Terraform Cloud authentication")
-	flags.NewStringFlag(multiregionCmd, flags.OrgAlternate, "", "Alternate Terraform Cloud organization")
-	flags.NewStringFlag(multiregionCmd, flags.TfcTokenAlternate, "", "Alternate token for Terraform Cloud")
+	flags.NewStringFlag(multiregionCmd, flags.OrgAlternate, "", "", "Alternate Terraform Cloud organization")
+	flags.NewStringFlag(multiregionCmd, flags.TfcTokenAlternate, "", "", "Alternate token for Terraform Cloud")
 }
 
 func outputFlagError(cmd *cobra.Command, err error) {
